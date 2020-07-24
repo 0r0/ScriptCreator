6d9191759fd8775120bbd446e4145db09b2e915d
0r0 <0r0.developer@gmail.com>
2020-07-24 12:06:17 +0430

    make done method for saving script

--- src/main/java/org/example/inputexcel/SaveScript.java
+++ src/main/java/org/example/inputexcel/SaveScript.java
@@ -1,9 +1,24 @@
 package org.example.inputexcel;
 
+import javafx.scene.control.FocusModel;
+
+import java.io.*;
+
 /**
  * saving generated script file in database or disk in file format ;)
  */
 public class SaveScript {
+    public static void done(StringBuilder scriptString,String issueName,String fileAddress) throws IOException {
+        //save script here
+        File scriptFile=new File(fileAddress+""+issueName+".sql");
+        FileOutputStream fileStream=new FileOutputStream(scriptFile);
+
+
+        fileStream.write(scriptString.toString().getBytes());
+        fileStream.flush();
+        fileStream.close();
+
+    }
 
 
 
